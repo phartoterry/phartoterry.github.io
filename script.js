7c96e296a53bfdc7cf83b456c8bf7fb49f1a0538
chien._.huang <114326790+phartoterry@users.noreply.github.com>
2024-12-14 09:56:32 +0800

    version 1

---
+++ script.js
@@ -0,0 +1,45 @@
+$(function() {
+    $('#submit').on('click', function() {
+      
+      // 姓名
+      var name = $('#demo_name').val() || '未填寫';
+
+      var relation = $('#relation').val() || '未填寫';
+
+      var num = $('#people-number').val() || '未填寫';
+
+      // 電子郵件
+      var mail = $('#demo_email').val() || '未填寫';
+
+      // vegetarian radio check
+      var is_vegetarian = $('[name="demo_veg"]').prop('checked') ? '是' : '否';
+
+      // need_child_seat radio check
+      var need_child_seat = $('[name="demo_child_chair"]').prop('checked') ? '是' : '否';
+
+      // need_invitation radio check
+      var need_invitation = $('[name="demo_invitation"]').prop('checked') ? '是' : '否';
+
+      // post
+      var data = {
+        'entry.2011968773': name,
+        'entry.1537783585': relation,
+        'entry.999905413': num,
+        'entry.1953885259': mail,
+        'entry.1476509680': is_vegetarian,
+        'entry.1197285354': need_child_seat,
+        'entry.955700446': need_invitation, 
+      };
+      $.ajax({
+        type: 'POST',
+        url: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSeyaMygKukmQTOcMA9hhXEOJse9a3lKdGAVSL2fXop38rDEOA/formResponse',
+        data: data,
+        contentType: 'application/json',
+        dataType: 'jsonp',
+        complete: function() {
+          alert('資料已送出！');
+        }
+      });
+      
+    });
+});
